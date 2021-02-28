6baa324168f57c3f09c884d4b46b42c2d1677b1d
jazelsug <jazels@nevada.unr.edu>
2021-02-28 15:56:08 -0800

    made deck class

--- blackjack.py
+++ blackjack.py
@@ -5,15 +5,17 @@ suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
 cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 # The card value
-cards_values = {"A":11, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}
+cards_values = {"A": 11, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7,
+                "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10}
 
 
+# CARD CLASS =============
 class Card:
-    def __init__(self, suit, val, cardval):
+    def __init__(self, suit, val, card_val):
         # 3rd param for value of card in blackjack?
         self.suit = suit
         self.value = val
-        self.card_value = cardval
+        self.card_value = card_val
 
     def show(self):
         # TODO: maybe add 2nd bool param for whether or not card should be displayed ?
@@ -21,6 +23,19 @@ class Card:
         print("{} of {}".format(self.value, self.suit))
 
 
+# DECK CLASS =========
+class Deck:
+    def __init__(self):
+        self.deck = []
+        self.build_deck()
+
+    def build_deck(self):
+        for suit in suits:
+            for card in cards:
+                self.deck.append(Card(suit, card, cards_values[card]))
+
+
+# DEALER CLASS ==================
 class Dealer:
     def __init__(self):
         # TODO: add parameter for deck/hand maybe?
@@ -33,6 +48,7 @@ class Dealer:
         pass
 
 
+# PLAYER CLASS ======================
 class Player:
     def __init__(self, first_bet = 0):
         # TODO: parameter for deck/hand maybe?
@@ -45,21 +61,15 @@ class Player:
     # TODO: make hit and stand functions? or make those Person methods + inheritance
 
 
+# BLACKJACK CLASS ===========================
 class BlackJack:
     def __init__(self):
         pass
 
 
-def build_deck():
-    deck = []
-    for suit in suits:
-        for card in cards:
-            deck.append(Card(suit, card, cards_values[card]))
-    return deck
-
-
+# MAIN FUNCTION =====================================
 if __name__ == "__main__":
-    dealer_deck = build_deck()
+    game_deck = Deck()
 
-    for x in dealer_deck:
+    for x in game_deck.deck:
         x.show()
