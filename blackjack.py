c0a3d933194306fcb3aa30f4a8e2207eec26cb1b
jazelsug <jazels@nevada.unr.edu>
2021-02-28 12:05:15 -0800

    made initial Player and Dealer classes

--- blackjack.py
+++ blackjack.py
@@ -21,6 +21,30 @@ class Card:
         print("{} of {}".format(self.value, self.suit))
 
 
+class Dealer:
+    def __init__(self):
+        # TODO: add parameter for deck/hand maybe?
+        pass
+
+    def shuffle(self):
+        pass
+
+    def deal_card(self):
+        pass
+
+
+class Player:
+    def __init__(self, first_bet = 0):
+        # TODO: parameter for deck/hand maybe?
+        self.bet = first_bet
+        pass
+
+    def bet(self, amount):
+        self.bet = amount
+
+    # TODO: make hit and stand functions? or make those Person methods + inheritance
+
+
 class BlackJack:
     def __init__(self):
         pass
