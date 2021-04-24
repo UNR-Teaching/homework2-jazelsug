cecb132aca58988bbce7b5b2beb29753fd639ae4
jazelsug <jazels@nevada.unr.edu>
2021-04-24 15:43:55 -0700

    documentation for Player

--- player.py
+++ player.py
@@ -2,6 +2,14 @@ from person import Person
 
 
 class Player(Person):
+    """User-controlled Player who plays against the Dealer in
+    the Blackjack game. Inherits from Person.
+
+    Parameters
+    ----------
+    d : Deck object
+        The Deck played with in the Blackjack game.
+    """
     def __init__(self, d):
         Person.__init__(self, d)
         # TODO: player bets?
@@ -9,6 +17,10 @@ class Player(Person):
     # hit is the same as in Person
 
     def show_hand(self):
+        """Function for printing to terminal the Player's hand.
+        Utilizes the show() method in card.py.
+        Overrides the show_hand() method in person.py.
+        """
         print("PLAYER'S HAND:")
         for h in self.hand:
             h.show()
