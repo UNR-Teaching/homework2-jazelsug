5927e430609456a8748ddab1a407fab491c44f08
jazelsug <jazels@nevada.unr.edu>
2021-04-24 16:02:34 -0700

    documentation for Blackjack

--- blackjack.py
+++ blackjack.py
@@ -5,12 +5,40 @@ from player import Player
 
 # BLACKJACK CLASS ===========================
 class BlackJack:
+    """Class for organizing and running the Blackjack game.
+
+    Parameters
+    ----------
+    p : Player object
+        An external Player created to play Blackjack.
+    d : Dealer object
+        An external Dealer created to play and deal cards in Blackjack.
+    w : str, default = None
+        The winner of the Blackjack game.
+    
+    Attributes
+    ----------
+    player : Player object
+        The Player playing the Blackjack game.
+    dealer : Dealer object
+        The Dealer playing in the Blackjack game.
+    winner : string
+        Holds the winner of the Blackjack game. Initialized to None.
+        Equals "Player" if the user-controlled Player wins the game,
+        "Dealer" if the computer dealer wins the game, or "Nobody" if
+        the Player and the Dealer ties.
+    """
     def __init__(self, p, d, w=None):
         self.player = p
         self.dealer = d
         self.winner = w
 
     def deal(self):
+        """Function for drawing the first 4 cards of the game.
+        The Player draws a face-up card, the Dealer draws a face-up card,
+        the Player draws another face-up card, then the Dealer draws
+        a face-down card. Hands are shown (printed to terminal) after each draw.
+        """
         # draw first 4 cards of the game
         self.player.hit(True)
         self.player.show_hand()
@@ -26,6 +54,9 @@ class BlackJack:
         print("\n")
 
     def get_winner(self):
+        """Function for determining the winner of the Blackjack game.
+        Person with higher score wins. A tied games means nobody wins.
+        """
         if self.player.score > self.dealer.score:
             self.winner = "Player"
         elif self.player.score < self.dealer.score:
@@ -36,11 +67,18 @@ class BlackJack:
             self.winner = None
 
     def show_all_face_up(self):
+        """Function for showing the hands of the Dealer and
+        the Player. Utilizes respective show_hand methods.
+        """
         self.dealer.show_hand()
         print("\n")
         self.player.show_hand()
 
     def reveal_all(self):
+        """Function for turning all cards in both the Dealer and Player's
+        hands face up, then showing the upturned hands. Utilizes
+        respective show_hand methods.
+        """
         print("\n")
         self.dealer.flip_cards()
         self.dealer.show_hand()
@@ -48,6 +86,13 @@ class BlackJack:
         self.player.show_hand()
 
     def check_ties(self):
+        """Function for checking for ties.
+        
+        Returns
+        -------
+        True if Dealer and Player's scores are equal,
+        False if Dealer and Player's scores are not equal.
+        """
         self.reveal_all()
         if self.player.score == self.dealer.score:
             return True
@@ -55,6 +100,16 @@ class BlackJack:
             return False
 
     def play_game(self):
+        """Function for playing the Blackjack game. After shuffling
+        and dealing, check for naturals. Then continue the game,
+        allowing the user-controlled Player to hit and stand as
+        desired. Continue playing until a winner is decided.
+
+        Returns
+        -------
+        winner : str
+            The entity that won the Blackjack game.
+        """
         print("LET'S PLAY!\nShuffling deck...\n")
         self.dealer.dealer_shuffle()
         self.deal()
