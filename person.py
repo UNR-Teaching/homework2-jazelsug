97d526203e367c5bfa2e5b498936a91aba915177
jazelsug <jazels@nevada.unr.edu>
2021-04-24 15:06:53 -0700

    documentation for Person

--- person.py
+++ person.py
@@ -2,12 +2,38 @@ import random
 
 
 class Person:
+    """Base class for Dealer and Player.
+
+    Parameters
+    ----------
+    d : Deck object
+        A Deck object to be used in the Blackjack game.
+    
+    Attributes
+    ----------
+    hand : list of Card objects
+        The cards that the person (dealer or player) is holding.
+    score : int
+        The count of the person; the sum of the card values in their hand.
+    person_deck : Deck object
+        A Deck object being used in the Blackjack game. The Dealer and the
+        Player should be using the same Deck.
+    """
     def __init__(self, d):
         self.hand = []
         self.score = 0
         self.person_deck = d
 
     def hit(self, up):
+        """Function for hitting in a game of Blackjack.
+        Here, the Person is asking for another card in an attempt to
+        get closer to a count of 21, or even hit 21 exactly.
+
+        Parameters
+        ----------
+        up : bool
+            Indicates whether the added card should be face up or not.
+        """
         new_card = None
         while new_card is None or new_card.in_hand:
             new_card = random.choice(self.person_deck.deck)
@@ -17,6 +43,16 @@ class Person:
         self.get_score()
 
     def check_bust(self):
+        """Function for checking if the Person has busted (has gotten
+        a score larger than 21). We also check for a soft hand; if the
+        Person busts with an Ace worth 11 in their hand, they can switch
+        the Ace value to 1. This enables them to keep playing.
+
+        Returns
+        ----------
+        True if the Person has busted,
+        False if the Person has not yet busted.
+        """
         if self.score > 21:
             # see if Aces can split from 11 to 1
             for h in self.hand:
@@ -32,10 +68,15 @@ class Person:
             return False
 
     def stand(self):
-        # done drawing cards, calculate updated score of cards in hand
+        """Function to calculate the updated score of cards in the
+        Person's hand. At this point, the Person is done drawing cards.
+        """
         self.get_score()
 
     def get_score(self):
+        """Function for calculating the current score of face-up cards
+        in the Person's hand.
+        """
         s = 0
         for c in self.hand:
             if c.face_up:
@@ -43,10 +84,26 @@ class Person:
         self.score = s
 
     def check_naturals(self):
+        """Function for checking if the Person has a natural or Blackjack
+        (first two cards are an ace and a "ten-card", resulting in a score of 21).
+
+        Returns
+        -------
+        True if the Person has a natural,
+        False if the Person does not have a natural.
+        """
         if self.score == 21:
             return True
+        else:
+            return False
 
     def show_hand(self):
+        """Function for printing to terminal the Person's hand. Utilizes
+        the show() method in Card for each Card in the Person's hand.
+        Also prints the score of the Person.
+
+        Is overridden in Player and Dealer.
+        """
         print("PERSON'S HAND:")
         for h in self.hand:
             h.show()
